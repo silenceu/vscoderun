1f52ba885ebc4f3101d01051fd605ebb4ff337db
Silence.Yu <szyhs24546@163.com>
2016-07-16 14:33:18 +0800

    Create runcode.bat
    run on windows

---
+++ runcode.bat
@@ -0,0 +1,27 @@
+@echo off
+set filename=%1
+set basename=%~n1
+set extname=%~x1
+set var=0
+IF "%extname%" == ".cpp" (set var=1)
+IF "%extname%" == ".c" (set var=2)
+IF "%extname%" == ".py" (set var=3)
+IF "%extname%" == ".java" (set var=4)
+GOTO CASE_%var%
+:CASE_0
+    ECHO Unsupported File Type!
+    GOTO END_SWITCH
+:CASE_1
+    g++ -std=c++11 -Wall %filename% -o %basename%.exe && %basename%.exe
+    GOTO END_SWITCH
+:CASE_2
+    gcc -std=c99 -Wall %filename% -o %basename%.exe && %basename%.exe
+    GOTO END_SWITCH    
+:CASE_3
+    py %filename%
+    GOTO END_SWITCH    
+:CASE_4
+    javac %filename% && java %basename%
+:END_SWITCH
+pause
+exit
