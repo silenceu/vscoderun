ee00c787543d6ad8b98d350a0bcdad7c9c61dcee
silenceu <szyhs24546@gmail.com>
2016-11-29 14:09:15 +0800

    Update vscoderun.sh
    fix error on macOS

--- vscoderun.sh
+++ vscoderun.sh
@@ -2,23 +2,25 @@
 filename=$1;
 extname=${filename##*.};
 if [ "$extname" = "cpp" ] ; then
-  bname=`basename "$filename" .cpp`
-  g++ -std=c++11 -Wall "$filename" -o "$bname" && ./"$bname"
+    bname=`basename "$filename" .cpp`
+    g++ -std=c++11 -Wall "$filename" -o "$bname" && ./"$bname" && rm "$bname"
 elif [ "$extname" = "c" ] ; then
-  bname=`basename "$filename" .c`
-  gcc -std=c99 -Wall "$filename" -o "$bname" && ./"$bname"
+    bname=`basename "$filename" .c`
+    gcc -std=c99 -Wall "$filename" -o "$bname" && ./"$bname" && rm "$bname"
 elif [ "$extname" = "py" ] ; then
-  python3 "$filename"
+    python3 "$filename"
 elif [ "$extname" = "java" ] ; then
-  bname=`basename "$filename" .java`
-  javac "$filename" && java "$bname"
+    bname=`basename "$filename" .java`
+    javac "$filename" && java "$bname" && rm "$bname".class
 elif [ "$extname" = "html" ] ; then
-  google-chrome "$filename"
-  exit
+    open -a Google\ Chrome "$filename"
+    exit
 elif [ "$extname" = 'cs' ] ; then
-  dotnet run
+    dotnet run
 else
- echo "unsupported file type!"
+   echo "unsupported file type!"
 fi
 echo -e "\n=========================================="
 read -n1 -p 'Program is complete,press any key to exit.'
+exit
+
